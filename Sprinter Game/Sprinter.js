ae7866f14d97b3976a17c7a0154782f0692a8064
Kaelinator <kirkkael@gmail.com>
2017-08-19 22:15:25 -0500

    Refactored Sprinter Game

--- Sprinter Game/Sprinter.js
+++ Sprinter Game/Sprinter.js
@@ -1,72 +1,95 @@
-function Sprinter(s, c) {
+function Sprinter(skill, color) {
 
-  this.distance = 0;
-  this.speed = 0;
-  this.vel = 0;
+  this.distance = 0; // distance traveled by the runner
+  this.speed = 0; // acceleration (amount of velocity gained each "stride")
+  this.velocity = 0; // "momentum"
 
-  this.skill = s;
+	/** AI variables */
+  this.skill = skill; // chance of correctly striding
+  this.previousKey = null;
 
-  this.prevKey = null;
-
-  this.c = c;
-  this.finished = false;
-  this.time = 0;
+  this.color = color;
+  this.finished = false; // whether to recieve input or draw the time
+  this.time = 0; // stopwatch
 }
 
-Sprinter.prototype.draw = function(lane) {
+/**
+ * draws the runner
+ */
+Sprinter.prototype.draw = function(lane, laneWidth) {
 
-  var y = (lane * w) + (w / 2);
+  var y = (lane * laneWidth) + (laneWidth / 2); // figure out y
 
-  fill(this.c);
+  fill(this.color);
   stroke(255);
   strokeWeight(2);
-  ellipse(this.distance, y, w / 2);
+  ellipse(this.distance, y, laneWidth / 2);
 
   if (this.finished) {
+		// draw the time
 
-    textSize(w/2);
+    textSize(laneWidth / 2);
     noStroke();
     fill(255);
     textAlign(RIGHT);
-    text((this.time / 1000).toFixed(2), width - w, y);
+    text((this.time / 1000).toFixed(2), width - laneWidth, y);
   }
 };
 
+/**
+ * handles distance based upon velocity,
+ * handles velocity based upon speed & resistance
+ * handles finish, stops stopwatch
+ */
 Sprinter.prototype.update = function() {
 
-  if (this.finished)
-    return;
-  else {
+  if (this.finished) {
+		// don't allow movement
+		return;
+	} else {
 
-    if (this.finished = (this.distance > width))
-      this.time = new Date().getTime() - startTime;
+    if (this.finished = (this.distance > width)) // set whether the runner finished the race
+      this.time = new Date().getTime() - startTime; // stop stopwatch
   }
 
-  this.vel = (this.vel > 3) ? 3 : this.vel;
+	/* move the Sprinter */
+  this.velocity = (this.velocity > 3) ? 3 : this.velocity; // constrain velocity
 
-  this.distance += this.vel;
-  this.vel += this.speed;
-  this.vel *= 0.99;
-  this.speed = 0;
+  this.distance += this.velocity; // movement
+  this.velocity += this.speed; // acceleration
+  this.velocity *= 0.99; // resistance
+  this.speed = 0; // reset acceleration
 };
 
+/**
+ * handles key press & previousKey
+ * accelerates the runner
+ */
 Sprinter.prototype.run = function(key) {
 
-  if (this.prevKey != null) {
+  if (this.previousKey != null) { // only if they've strided in the past
 
-    if (this.prevKey !== key && (this.prevKey + key === 76)) {
+		// LEFT_ARROW + RIGHT_ARROW = 76, therefore;
+		// 76 - LEFT_ARROW = RIGHT_ARROW &
+		// 76 - RIGHT_ARROW = LEFT_ARROW
+    if (this.previousKey != key && (this.previousKey + key === 76)) {
+			// they've tapped the correct key
 
-      this.speed += (this.vel / 75 + 0.06);
+      this.speed += (this.velocity / 75 + 0.06); // accelerate
     } else {
+			// they've pressed the wrong key
 
+			/* cut off momentum */
       this.speed = 0;
-      this.vel = 0;
+      this.velocity = 0;
     }
 
   } else {
+		// no previous key press
 
-    this.speed += (this.vel / 75 + 0.06);
+    this.speed += (this.velocity / 75 + 0.06);
   }
 
-  this.prevKey = key;
+	// keep track of key
+  this.previousKey = key;
 };
--- Sprinter Game/SprinterGame.js
+++ Sprinter Game/SprinterGame.js
@@ -1,63 +1,86 @@
 
-var runners = [];
-var runner;
+const SPRINTER_COUNT = 7; // how many lanes there are, including the player
 
-var w;
+var runners = []; // lineup
+var runner; // player
 
-var startTime;
+var laneWidth; // width of each lane
+
+var startTime; // beginning of the game
 
 function setup() {
-  createCanvas(680, 400);
+  createCanvas(window.innerWidth, window.innerHeight);
 
-  var c = rCol();
-  for (var i = 0; i < 6; i++) {
-
-    runners.push(new Sprinter(random(0.075) + 0.1, c));
+	/* initialize opponents */
+  var opponentColor = randomColor(); // color of opponents
+  for (var i = 1; i < SPRINTER_COUNT; i++) {
+		// push opponents
+    runners.push(new Sprinter(random(0.075) + 0.1, opponentColor));
   }
 
-  runner = new Sprinter(0, rCol());
+	/* initialize player */
+  runner = new Sprinter(0, invertColor(opponentColor));
   runners.splice(Math.floor(runners.length / 2), 0, runner);
 
-
+	/* initialize stopwatch */
   startTime = new Date().getTime();
-  w = height / runners.length;
+  laneWidth = height / runners.length;
 }
 
 function draw() {
   background(51);
 
-  drawTrack();
+  handleTrack();
   stride();
 }
 
+/**
+ * handle user input
+ */
 function keyPressed() {
 
   runner.run(keyCode);
 }
 
+/**
+ * AI for opponents
+ */
 function stride() {
 
   for (var r = 0; r < runners.length; r++) {
+		// loop through runners
 
-    if (random() < runners[r].skill)
-      runners[r].run(76 - runners[r].prevKey);
+    if (random() < runners[r].skill) { // calculate the speed of the runner
+			// take a stride
+
+			// LEFT_ARROW + RIGHT_ARROW = 76, therefore;
+			// 76 - LEFT_ARROW = RIGHT_ARROW &
+			// 76 - RIGHT_ARROW = LEFT_ARROW
+			runners[r].run(76 - runners[r].previousKey);
+		}
+
   }
 
 }
 
-function drawTrack() {
+/**
+ * draws & updates runners
+ * draws lanes
+ */
+function handleTrack() {
 
   for (var r = 0; r < runners.length; r++) {
 
-    runners[r].draw(r);
+		/* draw & update runners */
+    runners[r].draw(r, laneWidth);
     runners[r].update();
 
-    /* lanes */
-    stroke("#A14948");
-    strokeWeight(3);
+    /* draw lanes */
+    var y1 = (r / runners.length) * height; // inner line
+    var y2 = (r / runners.length) * height + laneWidth; // outer line
 
-    var y1 = (r / runners.length) * height;
-    var y2 = (r / runners.length) * height + w;
+		stroke("#A14948");
+		strokeWeight(3);
 
     line(0, y1, width, y1);
     line(0, y2, width, y2);
@@ -65,7 +88,22 @@ function drawTrack() {
 
 }
 
-function rCol() {
+/**
+ * returns a random color
+ */
+function randomColor() {
 
   return color(random(255), random(255), random(255));
+}
+
+/**
+ * returns an inverted color of the passed col
+ */
+function invertColor(col) {
+
+	var r = 255 - red(col);
+	var g = 255 - green(col);
+	var b = 255 - blue(col);
+
+	return color(r, g, b);
 }
